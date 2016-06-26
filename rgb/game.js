e02fac2690a13e80cd7df20f0904d863ccd128be
Ruyi Li <ruyili2002@gmail.com>
2016-06-26 16:14:29 -0400

    Update game.js

--- rgb/game.js
+++ rgb/game.js
@@ -2,10 +2,6 @@ canvas = document.getElementById("gameCanvas");
 ctx = canvas.getContext("2d");
 
 var player = new Image();
-var red = "red.png";
-var blue = "blue.png";
-var green = "green.png";
-var purple = "purple.png";
 var keysDown = {};
 var colory;
 var color;
@@ -37,18 +33,18 @@ function renderFrame() {
 	requestAnimationFrame(renderFrame);
 	
 	if (37 && 39 in keysDown){
-		player.src = "purple.png";
+		player.src = "https://ruyili.github.io/rgb/purple.png";
 		pcolor = "#800080"
 	}
 	
 	if(37 in keysDown && !(39 in keysDown)){
-		player.src = "red.png";
+		player.src = "https://ruyili.github.io/rgb/red.png";
 		pcolor = "#FF0000"
 	}else if (39 in keysDown && !(37 in keysDown)){
-		player.src = "blue.png";	
+		player.src = "https://ruyili.github.io/rgb/blue.png";	
 		pcolor = "#0000FF"
 	}else if (!(37 in keysDown) && !(39 in keysDown)){
-		player.src = "green.png";	
+		player.src = "https://ruyili.github.io/rgb/green.png";	
 		pcolor = "#00FF00"
 	}
 	document.body.style.backgroundColor = pcolor;
