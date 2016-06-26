0d097247d14b1c42b5538c24096e941316c2490f
Ruyi Li <ruyili2002@gmail.com>
2016-06-26 17:45:19 -0400

    Update main.js

--- cssshockwaveplease/main.js
+++ cssshockwaveplease/main.js
@@ -1,5 +1,5 @@
 var hex, dia, rad, dur, x, y, endless, res;
-var isHex;
+var isHex, prevres, halfw, halfh;
 
 
 
@@ -11,20 +11,29 @@ function magic(){
     x = parseInt(document.getElementById("xInput").value);
     y = parseInt(document.getElementById("yInput").value);
     endless = document.getElementById('infiniteCheck').checked;
+    halfw = document.getElementById("prev").offsetWidth/2;
+    halfh = document.getElementById("prev").offsetHeight/2;
     isHex  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test('#' + hex    );
+    
     
     if (isHex){
         res = "@keyframes generated{\n   0%{\n      width: 0px;\n      height: 0px;\n      opacity: 1;\n      top: " + y + "px;\n      left: " + x + "px;\n   }\n   100%{\n      width: " + dia + "px;\n      height: " + dia + "px;\n      opacity: 0;\n      top: " + (y - rad) + "px;\n      left: " + (x - rad) + "px;\n   }\n}\n\n#shockwave{\n   position: fixed;\n   top: -9999px;\n   left: -9999px;\n   width: " + dia + "px;\n   height: " + dia + "px;\n   border-radius: " + dia + "px;\n   background-color: #" + hex + ";\n   animation: generated " + dur + "s";
-    
+        
+        
+        prevres = "@keyframes generated{\n   0%{\n      width: 0px;\n      height: 0px;\n      opacity: 1;\n      top: " + halfh + "px;\n      left: " + halfw + "px;\n   }\n   100%{\n      width: " + dia + "px;\n      height: " + dia + "px;\n      opacity: 0;\n      top: " + (halfh - rad) + "px;\n      left: " + (halfw - rad) + "px;\n   }\n}\n\n#shockwave{\n   position: relative;\n   top: -9999px;\n   left: -9999px;\n   width: " + dia + "px;\n   height: " + dia + "px;\n   border-radius: " + dia + "px;\n   background-color: #" + hex + ";\n   animation: generated " + dur + "s";
+        
+        
         if(endless){
             res += " infinite;\n}";
+            prevres += " infinite;\n}";
         }else{
             res += ";\n}"
+            prevres += ";\n}"
         }
 
         var pre = document.getElementById('shockwave');
         var preStyle = document.getElementsByTagName('style')[0];
-        preStyle.innerHTML = res;
+        preStyle.innerHTML = prevres;
     }else{
         res = "Invalid hex code!";
     }   
