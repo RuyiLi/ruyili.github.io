0953d5fb75d9e52b12233878bcd8177a7c8925ad
Ruyi Li <ruyili2002@gmail.com>
2016-06-26 17:30:30 -0400

    Update main.js

--- cssshockwaveplease/main.js
+++ cssshockwaveplease/main.js
@@ -14,7 +14,7 @@ function magic(){
     isHex  = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test('#' + hex    );
     
     if (isHex){
-        res = "@keyframes generated{\n   0%{\n      width: 0px;\n      height: 0px;\n      opacity: 1;\n      top: " + y + "px;\n      left: " + x + "px;\n   }\n   100%{\n      width: " + dia + "px;\n      height: " + dia + "px;\n      opacity: 0;\n      top: " + (y - rad) + "px;\n      left: " + (x - rad) + "px;\n   }\n}\n\n#shockwave{\n   position: relative;\n   top: -9999px;\n   left: -9999px;\n   width: " + dia + "px;\n   height: " + dia + "px;\n   border-radius: " + dia + "px;\n   background-color: #" + hex + ";\n   animation: generated " + dur + "s";
+        res = "@keyframes generated{\n   0%{\n      width: 0px;\n      height: 0px;\n      opacity: 1;\n      top: " + y + "px;\n      left: " + x + "px;\n   }\n   100%{\n      width: " + dia + "px;\n      height: " + dia + "px;\n      opacity: 0;\n      top: " + (y - rad) + "px;\n      left: " + (x - rad) + "px;\n   }\n}\n\n#shockwave{\n   position: fixed;\n   top: -9999px;\n   left: -9999px;\n   width: " + dia + "px;\n   height: " + dia + "px;\n   border-radius: " + dia + "px;\n   background-color: #" + hex + ";\n   animation: generated " + dur + "s";
     
         if(endless){
             res += " infinite;\n}";
@@ -32,3 +32,30 @@ function magic(){
     document.getElementById("codeResult").value = res;
     
 }
+/*
+@keyframes example {
+    0%{
+      width: 1px;
+      height: 1px;
+      opacity: 1;
+    }
+    100%{
+      width: 190px;
+      height: 190px;
+      opacity: 0;
+      top: 95px;
+      left: -45px;
+    }
+}
+
+.xd {
+    position: fixed;
+    top: 190px;
+    left: 50px;
+    width: 190px;
+    height: 190px;
+    border-radius: 125px;
+    background-color: red;
+    animation: example 1s infinite;
+}
+*/
