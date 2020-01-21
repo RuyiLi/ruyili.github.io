5a1c7505dcc78781fb64340d13e4632e2b0afccf
Ruyi Li <ruyili2002@gmail.com>
2020-01-21 11:19:19 -0500

    Add link to Resumé

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const $ = document.querySelector.bind(document);
 
 function init () {
     new Swup({
-        linkSelector: 'a[href^="/"]',
+        linkSelector: 'a[href^="/"]:not(a[href^="/resume"])',
         debug: true
     });
 }
